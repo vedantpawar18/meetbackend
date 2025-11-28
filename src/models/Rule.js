a47f04968a535517a1f19329e1fc7903d7ef57ec
meetsethiya16 <meet.sethiya.16@gmail.com>
2025-11-28 21:57:44 +0530

    refactor Rule.js for improved consistency in string quotes

--- src/models/Rule.js
+++ src/models/Rule.js
@@ -1,16 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-/*
-  Basic Rule document structure.
-  type: 'weight'|'value'|custom
-  config: an object (for weight -> buckets)
-*/
-const ruleSchema = new mongoose.Schema({
-  name: String,
-  type: String,
-  priority: { type: Number, default: 10 },
-  config: { type: mongoose.Schema.Types.Mixed },
-  version: { type: String, default: '1.0' }
-}, { timestamps: true });
+const ruleSchema = new mongoose.Schema(
+  {
+    name: String,
+    type: String,
+    priority: { type: Number, default: 10 },
+    config: { type: mongoose.Schema.Types.Mixed },
+    version: { type: String, default: "1.0" },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Rule', ruleSchema);
+module.exports = mongoose.model("Rule", ruleSchema);
