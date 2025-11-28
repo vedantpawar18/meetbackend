becfe277ad6ba7ef9ec86d900521f661475b2abd
meetsethiya16 <meet.sethiya.16@gmail.com>
2025-11-28 21:50:25 +0530

    refactor Department.js for improved readability and structure

--- src/models/Department.js
+++ src/models/Department.js
@@ -1,3 +1,13 @@
-const mongoose = require('mongoose');
-const deptSchema = new mongoose.Schema({ name: { type: String, required: true }, description: String }, { timestamps: true });
-module.exports = mongoose.model('Department', deptSchema);
+const mongoose = require("mongoose");
+
+const deptSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Department", deptSchema);
